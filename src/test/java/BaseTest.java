a6c0a4cf9fa36c02dd76fc7fc1c403903fd4ad72
twachowicz <twachowicz@sii.pl>
2023-03-08 12:26:36 +0100

    solution version 1.2 - working in parallel

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
 
-    protected static WebDriver driver;
+    protected WebDriver driver;
 
 
     @BeforeAll
--- src/test/java/TitleTest.java
+++ src/test/java/TitleTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.parallel.ResourceLock;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -10,7 +9,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class TitleTest extends BaseTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/testData.csv")
-    @ResourceLock(value = "resources")
     @Tag(TestConstants.REGRESSION)
     void shouldValidateSiiPortalWebPageTitle(String url, String expectedTitle) {
         driver.get(url);
